3f46e558b138fe701138798daa63e3ba32dc1a9f
hamzaKhan2004 <khanhamzatz@gmail.com>
2026-09-09 23:24:02 +0530

    feat: chats api integrated

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,14 @@ import "dotenv/config";
 import app from "./src/app.js";
 import http from "http";
 import connectDB from "./src/config/database.js";
-import { initSocket } from "./src/sockets/server.socket.js";
+// import { initSocket } from "./src/sockets/server.socket.js";
 
 const port = process.env.PORT || 3000;
 connectDB();
 
-const httpServer = http.createServer(app);
-initSocket(httpServer);
+// const httpServer = http.createServer(app);
+// initSocket(httpServer);
 
-httpServer.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
