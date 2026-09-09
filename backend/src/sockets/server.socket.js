2af7a90f13ec27b8f02b1578fb1e60c03cdccb03
hamzaKhan2004 <khanhamzatz@gmail.com>
2026-09-10 05:17:50 +0530

    feat: chat UI integrated

--- backend/src/sockets/server.socket.js
+++ backend/src/sockets/server.socket.js
@@ -1,26 +1,26 @@
-// import { Server } from "socket.io";
+import { Server } from "socket.io";
 
-// let io;
+let io;
 
-// export function initSocket(httpServer) {
-//     io = new Server(httpServer, {
-//         cors: {
-//             origin: "http://localhost:5173",
-//             credentials: true,
-//         }
-//     });
+export function initSocket(httpServer) {
+    io = new Server(httpServer, {
+        cors: {
+            origin: "http://localhost:5173",
+            credentials: true,
+        }
+    });
 
-//     console.log("Socket IO Server is running...");
+    console.log("Socket IO Server is running...");
 
-//     io.on("connection", (socket) => {
-//         console.log("A user connected : ", socket.id);
-//     })
-// }
+    io.on("connection", (socket) => {
+        console.log("A user connected : ", socket.id);
+    })
+}
 
-// export function getIO() {
-//     if (!io) {
-//         throw new Error("Socket.io not initialized");
-//     }
+export function getIO() {
+    if (!io) {
+        throw new Error("Socket.io not initialized");
+    }
 
-//     return io;
-// }
+    return io;
+}
